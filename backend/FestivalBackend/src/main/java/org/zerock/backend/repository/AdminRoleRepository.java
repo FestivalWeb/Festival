accfc9b0c06978320008c6bafbf7a553c676b556
potibeo <dogsond5@naver.com>
2025-11-13 10:12:01 +0900

    엔티티 and 리포지토리 추가

--- backend/FestivalBackend/src/main/java/org/zerock/backend/repository/AdminRoleRepository.java
+++ backend/FestivalBackend/src/main/java/org/zerock/backend/repository/AdminRoleRepository.java
@@ -3,10 +3,6 @@ package org.zerock.backend.repository;
 import org.zerock.backend.entity.AdminRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * AdminRole 엔티티를 위한 리포지토리
- *
- * JpaRepository<[엔티티 클래스], [엔티티의 ID 클래스 타입]>
- */
+
 public interface AdminRoleRepository extends JpaRepository<AdminRole, AdminRole.AdminRoleId> {
 }
