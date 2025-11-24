0285b481b0cf7e08b36b0d57efc73623800a3efa
potibeo <dogsond5@naver.com>
2025-11-24 09:45:24 +0900

    지도, 날씨 API

--- backend/FestivalBackend/src/main/java/org/zerock/backend/config/WebConfig.java
+++ backend/FestivalBackend/src/main/java/org/zerock/backend/config/WebConfig.java
@@ -1,6 +1,7 @@
 package org.zerock.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
      * (test.html 같은 외부에서의 API 요청을 허용)
      */
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**") // 1. 모든 API 경로(/users/**, /map/** 등)에 대해
                 // 2. test.html 파일이 열린 주소(로컬 파일 'null' 또는 Live Server 'http://127.0.0.1:5500')를 허용
                 .allowedOrigins("http://127.0.0.1:5500", "null") 
