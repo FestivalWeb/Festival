e2f068e6ee35d6aebd15293515438a02b905c64c
jsbsb0204 <jsbsb0204@gmail.com>
2025-12-23 11:07:50 +0900

    boothData 예약안내문구 변경

--- frontend/frontend/src/components2/data/boothData.jsx
+++ frontend/frontend/src/components2/data/boothData.jsx
@@ -6,7 +6,7 @@ export const boothData = [
     img: "/images/booth1.jpg",
     detailImg: ["/images/booth1_1.jpg", "/images/booth1_1.jpg", "/images/booth1_1.jpg"],
     description: "논산딸기를 이용하여 케이크를 직접 만들어봅니다.",
-    detailText: "예약은 왼쪽 사이드바 하단의 체험부스 예약 탭을 눌러주세요."
+    detailText: "예약은 상단의 체험부스 버튼을 눌러주세요."
   },
   {
     id: 2,
@@ -14,7 +14,7 @@ export const boothData = [
     img: "/images/booth2.jpg",
     detailImg: ["/images/booth2_1.jpg"],
     description: "한국전통 디저트인 떡을 직접 만들어봅니다.",
-    detailText: "예약은 왼쪽 사이드바 하단의 체험부스 예약 탭을 눌러주세요."
+    detailText: "예약은 상단의 체험부스 버튼을 눌러주세요."
   },
   {
     id: 3,
@@ -22,7 +22,7 @@ export const boothData = [
     img: "/images/booth3.jpg",
     detailImg: ["/images/booth3_1.jpg"],
     description: "생딸기를 이용한 케이크·타르트·파르페 만들기 체험입니다.",
-    detailText: "예약은 왼쪽 사이드바 하단의 체험부스 예약 탭을 눌러주세요."
+    detailText: "예약은 상단의 체험부스 버튼을 눌러주세요."
   },
   {
     id: 4,
@@ -30,6 +30,6 @@ export const boothData = [
     img: "/images/booth4.jpg",
     detailImg: ["/images/booth3_1.jpg"],
     description: "논산 지역 농가가 직접 참여하는 농특산물 판매·홍보 부스입니다.",
-    detailText: "예약은 왼쪽 사이드바 하단의 체험부스 예약 탭을 눌러주세요."
+    detailText: "예약은 상단의 체험부스 버튼을 눌러주세요."
   }
 ];
